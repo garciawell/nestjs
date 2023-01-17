06745e4d2d26d7b582897d3dad8da7513df785e1
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-01-17 01:21:33 -0300

    interceptor and custom decorators

--- car-hub/src/users/users.controller.ts
+++ car-hub/src/users/users.controller.ts
@@ -9,8 +9,10 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -22,6 +24,7 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
