22e1286e0e5709eca59826ca48883d553c5d6b76
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-01-17 00:19:29 -0300

    create routes

--- car-hub/src/users/users.service.ts
+++ car-hub/src/users/users.service.ts
@@ -17,4 +17,33 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    return this.repo.remove(user);
+  }
 }
