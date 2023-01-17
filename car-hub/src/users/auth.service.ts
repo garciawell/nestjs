e6a1827f212cfcdaa7e289df76c74dfc93724fca
Wellington Garcia <garciawell@gmail.com>
2023-01-17 10:03:29 -0300

    Auth service

---
+++ car-hub/src/users/auth.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { promisify } from 'util';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    //
+    const users = await this.userService.find(email);
+
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.userService.create(email, result);
+
+    return user;
+  }
+
+  signin() {}
+}
