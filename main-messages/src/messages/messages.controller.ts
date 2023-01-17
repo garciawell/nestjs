05ab5f332e2ae3e643ba3726cc614cc639b2ef8b
Wellington Garcia <garciawell@gmail.com>
2023-01-16 21:44:25 -0300

    create controller repo service without Dependency injection

--- main-messages/src/messages/messages.controller.ts
+++ main-messages/src/messages/messages.controller.ts
@@ -1,21 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create.message.dto';
+import { MessagesService } from './messages.services';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    //
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
-    //
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
