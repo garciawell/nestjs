3c88f1079cf2f71eabfe1bacd8a86acd3ce3e307
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-01-19 20:00:51 -0300

    clear cookie

--- car-hub/src/users/users.controller.ts
+++ car-hub/src/users/users.controller.ts
@@ -36,12 +36,29 @@ export class UsersController {
   }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
   }
+
+  @Post('/signout')
+  async signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Get('/whoami')
+  async whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
+
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
   }
 
   @Get('/:id')
--- car-hub/src/users/users.service.ts
+++ car-hub/src/users/users.service.ts
@@ -19,6 +19,9 @@ export class UsersService {
   }
 
   findOne(id: number) {
+    if (!id) {
+      return null;
+    }
     return this.repo.findOneBy({ id });
   }
 
