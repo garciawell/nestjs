422f6d6da7972f47726ca33928a80cbc38e6580e
Wellington Garcia <garciawell@gmail.com>
2023-01-19 21:56:27 -0300

    test service

--- car-hub/src/users/auth.service.spec.ts
+++ car-hub/src/users/auth.service.spec.ts
@@ -67,10 +67,9 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await service.signup('asdf@asdf.com', '1234');
 
-    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+    await expect(service.signup('asdf@asdf.com', '1234')).rejects.toThrow(
       BadRequestException,
     );
   });
