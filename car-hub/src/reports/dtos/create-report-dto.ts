a97a3fab6c421c1aa1c68e76e4601d82d8c6b8ae
Wellington Garcia <garciawell@gmail.com>
2023-01-21 02:45:52 -0300

    dtos entity report

---
+++ car-hub/src/reports/dtos/create-report-dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Min(2050)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1_000_000)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+}
