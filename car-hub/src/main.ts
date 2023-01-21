793ba87eb35d8f72bd794a38c850d7768e493c49
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-01-21 02:18:05 -0300

    add e2e test and database local

--- car-hub/src/main.ts
+++ car-hub/src/main.ts
@@ -1,22 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(
-    cookieSession({
-      keys: ['test'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(8080);
 }
 bootstrap();
---
+++ car-hub/test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AuthenticationSystem (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const emailG = 'teste4@gmail.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: emailG,
+        password: '123456',
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(emailG);
+      });
+  });
+});
