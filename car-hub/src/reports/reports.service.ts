8a57fdaccb0c3457bf1ee9ffcd4246b916d6aecc
Wellington Garcia <garciawell@gmail.com>
2023-01-21 12:39:13 -0300

    reporty entity relation

--- car-hub/src/reports/reports.service.ts
+++ car-hub/src/reports/reports.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report-dto';
 import { Report } from './report.entity';
@@ -8,9 +9,26 @@ import { Report } from './report.entity';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(reportDto: CreateReportDto, user: User) {
     const reports = this.repo.create(reportDto);
+    reports.user = user;
 
     return this.repo.save(reports);
+  }
+
+  async changeApproval(id: string, isApproved: boolean) {
+    const report = await this.repo.findOne({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.isApproved = isApproved;
+
+    return this.repo.save(report);
   }
 }
