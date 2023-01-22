390200c9eeadc2b6581277068ab61d341d822b45
Wellington Garcia <garciawell@gmail.com>
2023-01-21 23:46:33 -0300

    fix: only approved

--- car-hub/src/reports/reports.service.ts
+++ car-hub/src/reports/reports.service.ts
@@ -42,6 +42,7 @@ export class ReportsService {
       .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
       .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
       .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('isApproved IS TRUE')
       .orderBy('ABS(mileage - :mileage)', 'DESC')
       .setParameters({
         mileage,
