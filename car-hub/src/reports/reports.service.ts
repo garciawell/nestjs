bb99fbadd1e32d90a4de4cd11e2804d355326da8
Wellington Garcia <garciawell@gmail.com>
2023-01-21 23:44:49 -0300

    Create QueryBuilder

--- car-hub/src/reports/reports.service.ts
+++ car-hub/src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report-dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './report.entity';
 
 @Injectable()
@@ -30,5 +31,22 @@ export class ReportsService {
     report.isApproved = isApproved;
 
     return this.repo.save(report);
+  }
+
+  createEstimate({ lng, lat, make, model, year, mileage }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({
+        mileage,
+      })
+      .limit(3)
+      .getRawOne();
   }
 }
