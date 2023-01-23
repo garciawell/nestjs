5781c53092583e5b4150d22d2b766d492367c0d4
Wellington Garcia <garciawell@gmail.com>
2023-01-22 21:09:58 -0300

    Final project deploy

--- car-hub/src/config/typeorm.config.ts
+++ car-hub/src/config/typeorm.config.ts
@@ -7,9 +7,21 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        type: 'postgres',
+        synchronize: false,
+        database: this.configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      };
+    }
     return {
       type: 'sqlite',
-      synchronize: false,
+      synchronize: process.env.NODE_ENV === 'test' ? true : false,
       database: this.configService.get<string>('DB_NAME'),
       autoLoadEntities: true,
     };
